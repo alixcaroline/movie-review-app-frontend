6781eb5d5ae6cafb5b973140e6b4612280b22ba2
alixcaroline <alixcaroline13@gmail.com>
2023-01-19 10:24:30 +0100

    signup user logic

--- src/components/user/auth/Signup.jsx
+++ src/components/user/auth/Signup.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUser } from '../../../api/auth';
 import { commonModelClasses } from '../../../utils/theme';
 import Container from '../../Container';
 import CustomLink from '../../CustomLink';
@@ -7,15 +8,80 @@ import FormInput from '../../form/FormInput';
 import Submit from '../../form/Submit';
 import Title from '../../form/Title';
 
+const validateUserInfo = ({ name, email, password }) => {
+	const isValidName = /^[a-z A-Z]+$/;
+	const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	if (!name.trim()) return { ok: false, error: 'Name is missing!' };
+	if (!isValidName.test(name)) return { ok: false, error: 'Invalid name' };
+
+	if (!email.trim()) return { ok: false, error: 'Email is missing!' };
+	if (!isValidEmail.test(email)) return { ok: false, error: 'Invalid email' };
+
+	if (!password.trim()) return { ok: false, error: 'Password is missing!' };
+	if (password.length < 8)
+		return { ok: false, error: 'Password must be 8 characters long!' };
+
+	return { ok: true };
+};
+
 const Signup = () => {
+	const [userInfo, setUserInfo] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
+
+	const { name, email, password } = userInfo;
+
+	const handleChange = ({ target }) => {
+		const { value, name } = target;
+		setUserInfo({ ...userInfo, [name]: value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const { ok, error } = validateUserInfo(userInfo);
+
+		if (!ok) return console.log(error);
+
+		const response = await createUser(userInfo);
+
+		if (response.error) return console.log(response.error);
+
+		console.log(response.user);
+	};
+
 	return (
 		<FormContainer>
 			<Container>
-				<form action='' className={commonModelClasses + ' w-72'}>
+				<form
+					onSubmit={handleSubmit}
+					action=''
+					className={commonModelClasses + ' w-72'}>
 					<Title>Sign up</Title>
-					<FormInput name='name' placeholder='John Doe' label='Name' />
-					<FormInput name='email' placeholder='john@email.com' label='Email' />
-					<FormInput name='password' placeholder='********' label='Password' />
+					<FormInput
+						onChange={handleChange}
+						value={name}
+						name='name'
+						placeholder='John Doe'
+						label='Name'
+					/>
+					<FormInput
+						onChange={handleChange}
+						value={email}
+						name='email'
+						placeholder='john@email.com'
+						label='Email'
+					/>
+					<FormInput
+						onChange={handleChange}
+						value={password}
+						name='password'
+						placeholder='********'
+						label='Password'
+						type='password'
+					/>
 					<Submit value='Sign up' />
 					<div className='flex justify-between'>
 						<CustomLink to='/auth/forget-password'>Forget password</CustomLink>
